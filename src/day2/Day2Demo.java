9cc0ab08d48ce7f469d59d70476ed68cff418866
Ruthman Santos <Ruthman Santos@10.0.0.73>
2023-04-07 20:54:58 -0400

    getId()...

--- src/day2/Day2Demo.java
+++ src/day2/Day2Demo.java
@@ -5,6 +5,11 @@ public class Day2Demo {
 	public void getInfo() {
 		System.out.println("Pushing code...");
 		System.out.println("Pushing code1...");
+			
+		}
+		
+	
+	public void getId() {
 		
 	}
 	
